0f36ecccfa8c905459a0b408b5ccd9e09bfaa82a
James <thisis.chenwei@gmail.com>
2025-07-05 23:51:57 +0800

    Update settings.py 設定法幣匯率以及車馬費

--- settings.py
+++ settings.py
@@ -64,7 +64,7 @@ SESSION_CONFIGS = [
 
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+    real_world_currency_per_point=2.00, participation_fee=150.00, doc=""
 )
 
 PARTICIPANT_FIELDS = []
