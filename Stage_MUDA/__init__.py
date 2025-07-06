1ad701777b341f48869ff35edb6b80b198c7132c
James <thisis.chenwei@gmail.com>
2025-07-06 11:03:25 +0800

    Update __init__.py

--- Stage_MUDA/__init__.py
+++ Stage_MUDA/__init__.py
@@ -481,6 +481,7 @@ def _calculate_final_payoff_info(player: Player) -> Dict[str, Any]:
         'profit': profit,
         'profit_formatted': f"{int(round(profit))}",
         'total_value_formatted': f"{int(round(total_value))}",
+        'initial_capital_formatted': f"{int(round(selected_round_player.initial_capital))}",
     }
 
 page_sequence = [Introduction, ReadyWaitPage, TradingMarket, ResultsWaitPage, Results]
