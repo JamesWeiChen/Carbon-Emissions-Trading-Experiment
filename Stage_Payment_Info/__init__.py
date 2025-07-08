13b3a7442142a45d2868323d8e9152d2680c7404
James <thisis.chenwei@gmail.com>
2025-07-08 11:13:52 +0800

    Update __init__.py

--- Stage_Payment_Info/__init__.py
+++ Stage_Payment_Info/__init__.py
@@ -84,7 +84,32 @@ class PaymentInfo(Page):
         player.payoff = cu(info['total_profit'])  # 必須是 cu() 才會統計進 payoff
         player.total_payment = info['total_payment']
 
+class BasicInfo(Page):
+    form_model = 'player'
+    form_fields = ['name', 'student_id', 'id_number', 'address', 'address_code']
+
+    @staticmethod
+    def error_message(player: Player, values):
+        if len(values['student_id']) != 9:
+            return '學號長度不正確'
+        if not values['student_id'][0].isalpha():
+            return '學號第 1 碼應為英文字母'
+        if not values['student_id'][1:2].isnumeric():
+            return '學號格式不正確'
+        if not values['student_id'][4:8].isnumeric():
+            return '學號格式不正確'
+
+        if len(values['id_number']) != 10:
+            return '身份證字號長度不正確'
+        if not values['id_number'][0].isalpha():
+            return '身份證字號第 1 碼應為英文字母'
+        if not values['id_number'][1:9].isnumeric():
+            return '身份證字號格式不正確'
+
+        if len(values['address_code']) != 3 or not values['address_code'].isnumeric():
+            return '戶籍地址郵遞區號應為 3 碼數字'
+
 class WaitForInstruction(Page):
     pass
 
-page_sequence = [PaymentInfo, WaitForInstruction]
+page_sequence = [PaymentInfo, BasicInfo, WaitForInstruction]
