3fbde1c04161439c8254ff9f9b6bc25446407551
James <thisis.chenwei@gmail.com>
2025-07-10 15:01:07 +0800

    Update __init__.py

--- Stage_Payment_Info/__init__.py
+++ Stage_Payment_Info/__init__.py
@@ -39,8 +39,8 @@ class Player(BasePlayer):
         )
 
         total_profit = control.get("profit", 0) + carbon.get("profit", 0)
-        total_emission = control.get("emission", 0) + carbon.get("emission", 0)
-        total_group_emission = control.get("group_emission", 0) + carbon.get("group_emission", 0)
+        total_emission = control.get("emission", 12) + carbon.get("emission", 11)
+        total_group_emission = control.get("group_emission", 50) + carbon.get("group_emission", 30)
         real_emission = total_group_emission * session.config.get("carbon_real_world_rate", 0.1)
 
         # 使用 cu() 包裝，才能使用 oTree 的貨幣轉換方法
