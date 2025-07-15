db282963ea7201621c933fc955f6a0fa18fc01a4
James <thisis.chenwei@gmail.com>
2025-07-15 18:45:23 +0800

    Update __init__.py

--- Stage_Control/__init__.py
+++ Stage_Control/__init__.py
@@ -71,7 +71,7 @@ class Player(BasePlayer):
     
     # 財務相關
     revenue = models.CurrencyField()
-    total_cost = models.FloatField()
+    models.DecimalField(decimal_places=2)
     net_profit = models.FloatField()
     initial_capital = models.CurrencyField()
     current_cash = models.CurrencyField()
