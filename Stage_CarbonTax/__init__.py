848ab2798b63eedce6fc6470d9849ab473f48677
James <thisis.chenwei@gmail.com>
2025-07-17 10:16:11 +0800

    Update __init__.py

--- Stage_CarbonTax/__init__.py
+++ Stage_CarbonTax/__init__.py
@@ -192,7 +192,7 @@ class Results(Page):
             
             # 格式化數值
             'production_cost_formatted': f"{int(round(production_cost))} 法幣",
-            'carbon_tax_formatted': f"{int(round(carbon_tax))}",
+            'carbon_tax_formatted': f"{int(round(carbon_tax))} 法幣",
             'revenue_formatted': f"{int(round(float(player.revenue)))}",
             'net_profit_formatted': f"{int(round(player.net_profit))} 法幣",
             'total_emissions_formatted': f"{int(round(total_emissions))}",
