fe813d747531230b8bd8e6e05fa7f2e6b6b37df6
James <thisis.chenwei@gmail.com>
2025-07-24 10:57:18 +0800

    Update __init__.py

--- Stage_Control/__init__.py
+++ Stage_Control/__init__.py
@@ -162,7 +162,8 @@ class Results(Page):
             'marginal_cost_coefficient': player.marginal_cost_coefficient,
             
             # 格式化數值
-            'production_cost_formatted': f"{int(round(production_cost))} 法幣",
+            #'production_cost_formatted': f"{int(round(production_cost))} 法幣",
+            'production_cost_formatted': f"{production_cost} 法幣",
             'revenue_formatted': f"{int(round(float(player.revenue)))}",
             'net_profit_formatted': f"{int(round(player.net_profit))} 法幣",
             'total_emissions_formatted': f"{int(round(total_emissions))}",
