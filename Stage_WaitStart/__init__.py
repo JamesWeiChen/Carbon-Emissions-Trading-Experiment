bdd80cab67d7d76e38043da6a40de33ef75224b3
James <thisis.chenwei@gmail.com>
2025-09-02 13:04:57 +0800

    Refactor Player class and Consent page
    Removed consent field from Player class and updated Consent page logic.

--- Stage_WaitStart/__init__.py
+++ Stage_WaitStart/__init__.py
@@ -12,18 +12,18 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
-    consent = models.BooleanField(label="本人已閱讀並同意上述資訊與條款", blank=False)
+    pass
+
 
 class Consent(Page):
-    form_model = 'player'
-    form_fields = ['consent']
-
-class WaitStart(Page):
     form_model = None
 
     @staticmethod
-    def is_displayed(player: Player):
-        # 僅在已同意時顯示
-        return player.consent is True
+    def vars_for_template(player: Player):
+        return {}
+
+
+class WaitStart(Page):
+    form_model = None
 
 page_sequence = [Consent, WaitStart]
